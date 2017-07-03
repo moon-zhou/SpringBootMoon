748d4c48bf4f32ad431c279211962b8b48f1425f
moon-zhou <ayimin1989@163.com>
2017-07-03 23:25:28 +0800

    spring boot
    thymeleaf template

--- src/main/java/com/moon/SpringBootMoon/controller/HelloWorldController.java
+++ src/main/java/com/moon/SpringBootMoon/controller/HelloWorldController.java
@@ -7,11 +7,14 @@ import com.moon.SpringBootMoon.domain.User;
 
 @RestController
 public class HelloWorldController {
+    
+    // http://localhost:8080/hello
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
     }
     
+    // http://localhost:8080/getUser
     @RequestMapping("/getUser")
     public User getUser() {
         User user=new User();
