d08a9108ae1c80731b8a753f4aa39cc8dcf57c72
moon-zhou <ayimin1989@163.com>
2017-07-13 21:49:16 +0800

    import redis
    no testing

--- src/main/java/com/moon/SpringBootMoon/controller/HelloWorldController.java
+++ src/main/java/com/moon/SpringBootMoon/controller/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.moon.SpringBootMoon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moon.SpringBootMoon.domain.User;
+import com.moon.SpringBootMoon.domain.UserRepository;
 
 @RestController
 public class HelloWorldController {
+    
+    @Autowired
+    private UserRepository userRepository;
     
     // http://localhost:8080/hello
     @RequestMapping("/hello")
@@ -17,9 +23,18 @@ public class HelloWorldController {
     // http://localhost:8080/getUser
     @RequestMapping("/getUser")
     public User getUser() {
+        
         User user=new User();
         user.setUserName("小明");
         user.setPassWord("xxxx");
+        return user;
+    }
+    
+    @RequestMapping("/getUserCache")
+    @Cacheable(value="user-key")
+    public User getUserCache() {
+        User user = userRepository.findByUserName("aa");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");  
         return user;
     }
 }
